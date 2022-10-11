c164f246ea066820544f8b96bd4735ca243e20a5
Nataliia Ilnytska <lelya_antonenko@bk.ru>
2022-10-11 22:06:13 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketprice = 7000;
+        int bonus = 20;
+
+        System.out.println("Количество бонусных миль:");
+        System.out.println(ticketprice / bonus);
+        
+
+
+
+
+    }
+}
